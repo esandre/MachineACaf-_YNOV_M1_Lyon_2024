b5eb1d9719fc9778c3432725dba5bfa297df07cd
Enzo Sandre <enzo@enzosandre.fr>
2024-07-04 11:30:51 +0200

    [RED] Cas nominal ne compilant pas

--- test/machineACafé.test.ts
+++ test/machineACafé.test.ts
@@ -1,5 +1,15 @@
-describe("Test", () => {
-    test("Test 1", () => {
-        expect(true).toBe(true);
+describe("MVP", () => {
+    test("Cas nominal", () => {
+        // ETANT DONNE une machine a café
+        let machineACafé = new MachineACafé()
+
+        // QUAND on insère 50cts
+        machineACafé.insérer(Pièce.CinquanteCentimes)
+
+        // ALORS il a été demandé au hardware de servir un café
+        expect(machineACafé.nombreCafésServis).toEqual(1);
+
+        // ET l'argent est encaissé
+        expect(machineACafé.argentEncaisséEnCentimes).toEqual(50);
     })
 })

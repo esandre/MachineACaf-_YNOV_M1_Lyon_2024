5e2ab179374716516ad7893c6f2089b6f5645d63
Enzo Sandre <enzo@enzosandre.fr>
2024-07-09 10:01:10 +0200

    Ajout de boutons

--- src/hardware/hardware.interface.ts
+++ src/hardware/hardware.interface.ts
@@ -79,8 +79,12 @@ export interface ButtonPanelInterface {
 
 export enum ButtonCodes {
     BTN_LUNGO = 0,
-    BTN_SUGAR = 1,
-    BTN_MILK = 2,
+    BTN_SUGAR_PLUS = 1,
+    BTN_SUGAR_MINUS = 4,
+    BTN_LATTE = 2,
+    BTN_CHOCOLATE_WATER = 5,
+    BTN_CHOCOLATE_MILK= 6,
+    BTN_CAPUCCINO = 7,
     BTN_MAINTENANCE_RESET = 3
 }
 
